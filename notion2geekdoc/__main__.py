c7fa4fc7308203c4e1ab5bd2f055927a5e5a8c46
hwangseonbi <on91716@gmail.com>
2021-06-04 20:08:35 +0900

    메인 디렉토리 이름 변경

--- notion2geekdoc/__main__.py
+++ notion2geekdoc/__main__.py
@@ -4,7 +4,6 @@ import click
 from converter import NotionConverter
 
 
-# python3 notion-sync --root "https://www.notion.so/hwangseonbi/Draft-699ba9fe0d5946bebd8ede8d7134b4f1"
 @click.command()
 @click.argument('root_page_url')
 @click.option('--content', default="./content", help='Where do you produce?')
